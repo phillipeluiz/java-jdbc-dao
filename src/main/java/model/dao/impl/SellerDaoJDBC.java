b537bed44aad23ce3338244522b1955e7f91d6f6
Phillipe <phillipeluiz@outlook.com>
2023-07-25 22:24:47 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,35 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC  implements SellerDao {
+
+    @Override
+    public void insert(Seller department) {
+
+    }
+
+    @Override
+    public void update(Seller department) {
+
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+
+    @Override
+    public Department findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
